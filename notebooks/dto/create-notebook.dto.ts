623957138bafa41a297f8861404aa29bb9dffe44
muirkajika <moore.823@gmail.com>
2025-07-06 16:20:46 -0400

    Update create-notebook.dto.ts
    correct export class

--- notebooks/dto/create-notebook.dto.ts
+++ notebooks/dto/create-notebook.dto.ts
@@ -1,18 +1,3 @@
-import {
-  Entity,
-  PrimaryGeneratedColumn,
-  Column,
-  CreateDateColumn,
-} from 'typeorm';
-
-@Entity('notebooks') // This will create a table named "notebooks"
-export class Notebook {
-  @PrimaryGeneratedColumn('uuid')
-  id: string;
-
-  @Column({ type: 'varchar', length: 255 })
+export class CreateNotebookDto {
   name: string;
-
-  @CreateDateColumn()
-  createdAt: Date;
 }
